8efbc24ce009d3863645ad806c817d291a734566
shuvo247 <mdshakibul.shuvo@gmail.com>
2022-10-02 11:13:36 +0600

    Master File

---
+++ plugin-boilarplate.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name: Plugin Boilarplate
+ * Description: A simple plugin with simple crud and ajax crud
+ * Plugin URi: https://github.com/shuvo247
+ * Author: MD Shakibul Islam
+ * Author URI: https://github.com/shuvo247
+ * Version: 1.0.0
+ * License: GPL2 or Later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ */
+
+ if( !defined( 'ABSPATH' ) ) {
+    exit();
+ }
